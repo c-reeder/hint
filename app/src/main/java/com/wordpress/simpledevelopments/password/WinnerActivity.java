4c5243ee5d92fcaa5fe41a1841bfb54261cc8e45
Connor Reeder <connor.reeder@ymail.com>
2019-12-21 21:29:57 -0300

    Ton of balloons with random pastel colors

--- app/src/main/java/com/wordpress/simpledevelopments/password/WinnerActivity.java
+++ app/src/main/java/com/wordpress/simpledevelopments/password/WinnerActivity.java
@@ -2,14 +2,23 @@ package com.wordpress.simpledevelopments.password;
 
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Build;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.transition.Explode;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
@@ -24,6 +33,8 @@ public class WinnerActivity extends AppCompatActivity {
     private static final String TAG = "WinnerActivity";
 
     private static final int NUM_ROUNDS = 6;
+
+    private final int NUM_BALLOONS = 16;
 
 
     @Override
@@ -51,31 +62,57 @@ public class WinnerActivity extends AppCompatActivity {
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                ImageView[] balloons = new ImageView[3];
-                int[] xs = new int[3];
-                ObjectAnimator[] animators = new ObjectAnimator[3];
 
+                // Calculate the number of pixels for a dimension of 100dp
+                int px = (int) TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        100,
+                        getResources().getDisplayMetrics()
+                );
 
-                balloons[0] = findViewById(R.id.greenBalloon);
-                balloons[1] = findViewById(R.id.redBalloon);
-                balloons[2] = findViewById(R.id.blueBalloon);
-
-                Random random = new Random();
+                // Get screen dimensions
                 Display display = getWindowManager().getDefaultDisplay();
                 Point size = new Point();
                 display.getRealSize(size);
                 float screenHeight = size.y;
                 float screenWidth = size.x;
 
-                for (int i = 0; i < 3; i++) {
-                    xs[i] = random.nextInt((int) (screenWidth - balloons[0].getWidth() + 1));
-                    balloons[i].setTranslationX(xs[i]);
-                    //balloons[i].setTranslationY(-1 * screenHeight);
-                    animators[i] = ObjectAnimator.ofFloat(balloons[i], "translationY",0, -1 * (screenHeight + balloons[0].getHeight()));
-                    animators[i].setDuration(5000);
-                    animators[i].setInterpolator(new LinearInterpolator());
-                    animators[i].setStartDelay(random.nextInt(4001));
-                    animators[i].start();
+
+                //ImageView[] balloons = new ImageView[NUM_BALLOONS];
+                //ObjectAnimator[] animators = new ObjectAnimator[NUM_BALLOONS];
+
+
+                Random random = new Random();
+                ConstraintLayout layout = findViewById(R.id.activity_winner);
+
+                for (int i = 0; i < NUM_BALLOONS; i++) {
+                    // Create Balloon ImageView with given size
+                    ImageView balloonView = new ImageView(getApplicationContext());
+                    balloonView.setId(View.generateViewId());
+                    ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(px,px);
+                    balloonView.setLayoutParams(params);
+
+                    // Create Drawable to add to ImageView
+                    LayerDrawable layerDrawable = (LayerDrawable) ContextCompat.getDrawable(getApplicationContext(), R.drawable.balloon_list);
+                    balloonView.setImageDrawable(layerDrawable);
+                    DrawableCompat.setTint(layerDrawable.getDrawable(0).mutate(),generateRandomPastelColor(random));
+
+                    // Add the Balloon ImageView at the default position
+                    // at X=0, Y=screenHeight (just below view)
+                    layout.addView(balloonView);
+                    balloonView.setTranslationY(screenHeight);
+
+
+                    // Give balloon random x value somewhere within the bounds of the screen
+                    balloonView.setTranslationX(random.nextInt((int) (screenWidth - px + 1)));
+
+                    //Animate balloon
+                    ObjectAnimator balloonAnimator = ObjectAnimator.ofFloat(balloonView, "translationY",screenHeight, -1 * px);
+                    balloonAnimator.setDuration(5000);
+                    balloonAnimator.setInterpolator(new LinearInterpolator());
+                    balloonAnimator.setStartDelay(500 * i);
+                    //balloonAnimator.setStartDelay(random.nextInt(10000));
+                    balloonAnimator.start();
                 }
 
             }
@@ -84,6 +121,15 @@ public class WinnerActivity extends AppCompatActivity {
         handler.postDelayed(runnable, 1000);
 
 
+    }
+    public int generateRandomPastelColor(Random rand) {
+        final int baseColor = Color.WHITE;
+
+        final int red = (Color.red(baseColor) + rand.nextInt(256)) / 2;
+        final int green = (Color.green(baseColor) + rand.nextInt(256)) / 2;
+        final int blue = (Color.blue(baseColor) + rand.nextInt(256)) / 2;
+
+        return Color.rgb(red, green, blue);
     }
     public void restartGame(View view) {
         //supportFinishAfterTransition();
